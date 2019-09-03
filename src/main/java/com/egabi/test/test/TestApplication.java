d4d3e0a349d981d594e379243d80369231a59796
ayman alsapagh <aymanalsayed49@gmail.com>
2019-09-03 13:43:03 +0200

    https://github.com/Alsapagh/test.git

---
+++ src/main/java/com/egabi/test/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.egabi.test.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+        System.out.println("ayyyyyman");
+        SpringApplication.run(TestApplication.class, args);
+	}
+
+}
