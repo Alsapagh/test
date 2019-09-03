96ca96e288baa6b15d1dcdd0652f665eaec6c37f
ayman alsapagh <aymanalsayed49@gmail.com>
2019-09-03 14:48:11 +0200

    First commit

--- src/main/java/com/egabi/test/test/TestApplication.java
+++ src/main/java/com/egabi/test/test/TestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
-        System.out.println("ayyyyyman");
         SpringApplication.run(TestApplication.class, args);
 	}
 
